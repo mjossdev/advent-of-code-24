38fb56c35c515b9e4800b9212bf85328916b3ad1
Marcel Joss <marceljoss@hotmail.ch>
2024-12-02 08:09:37 +0100

    Day 2 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -6,22 +6,32 @@ fun main() {
         line.split(' ').map { it.toInt() }
     }
 
-    fun part1(input: List<String>): Int {
-        val reports = readReports(input)
-        return reports.count { report ->
-            val sign = (report[1] - report[0]).sign
-            report.zipWithNext { a, b -> b - a }.all { it.sign == sign && abs(it) in 1..3 }
+    fun List<Int>.isSafe(): Boolean {
+        val sign = (this[1] - this[0]).sign
+        return zipWithNext { a, b -> b - a }.all { it.sign == sign && abs(it) in 1..3 }
+    }
+
+    fun List<Int>.isSafeWithDampener(): Boolean = isSafe() || indices.any { i ->
+        toMutableList().let {
+            it.removeAt(i)
+            it.isSafe()
         }
     }
 
+    fun part1(input: List<String>): Int {
+        val reports = readReports(input)
+        return reports.count { it.isSafe() }
+    }
+
     fun part2(input: List<String>): Int {
-        return 0
+        val reports = readReports(input)
+        return reports.count { it.isSafeWithDampener() }
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 2)
-//    check(part2(testInput) == 31)
+    check(part2(testInput) == 4)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day02")
