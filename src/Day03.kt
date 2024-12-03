05bc93dabd2af8c5ef9ae1a42a1b11b0a3b48fda
Marcel Joss <marceljoss@hotmail.ch>
2024-12-03 06:28:22 +0100

    Day 3 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,23 +1,42 @@
 fun main() {
     data class MulInstruction(val x: Int, val y: Int)
 
-    fun readInstructions(input: String): List<MulInstruction> {
-        val pattern = Regex("""mul\((\d+),(\d+)\)""")
-        return pattern.findAll(input).map { matchResult ->
-            val (x, y) = matchResult.groupValues.drop(1).map { it.toInt() }
-            MulInstruction(x, y)
-        }.toList()
-    }
+    fun part1(input: String): Int {
+        fun readInstructions(input: String): List<MulInstruction> {
+            val pattern = Regex("""mul\((\d+),(\d+)\)""")
+            return pattern.findAll(input).map { matchResult ->
+                val (x, y) = matchResult.groupValues.drop(1).map { it.toInt() }
+                MulInstruction(x, y)
+            }.toList()
+        }
 
-    fun part1(input: String): Int = readInstructions(input).sumOf { it.x * it.y }
+        return readInstructions(input).sumOf { it.x * it.y }
+    }
 
     fun part2(input: String): Int {
-        return 0
+        fun readInstructions(input: String): List<MulInstruction> = buildList {
+            val pattern = Regex("""mul\((\d+),(\d+)\)|do(?:n't)?\(\)""")
+            var enabled = true
+            pattern.findAll(input).forEach { matchResult ->
+                val value = matchResult.value
+                when {
+                    value == "do()" -> enabled = true
+                    value == "don't()" -> enabled = false
+                    value.startsWith("mul") -> {
+                        if (enabled) {
+                            val (x, y) = matchResult.groupValues.drop(1).map { it.toInt() }
+                            add(MulInstruction(x, y))
+                        }
+                    }
+                }
+            }
+        }
+
+        return readInstructions(input).sumOf { it.x * it.y }
     }
 
-    val testInput = readInputAsString("Day03_test")
-    check(part1(testInput) == 161)
-    check(part2(testInput) == 0)
+    check(part1("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))") == 161)
+    check(part2("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))") == 48)
 
     val input = readInputAsString("Day03")
     part1(input).println()
