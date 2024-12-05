7dda21f8dabbe406270529f7c4ca2a947156eaf7
Marcel Joss <marceljoss@hotmail.ch>
2024-12-05 15:59:37 +0100

    Day 4 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,6 +1,7 @@
 fun main() {
     data class Point(val row: Int, val col: Int)
     data class Vector(val rowDelta: Int, val colDelta: Int)
+
     operator fun Vector.times(factor: Int) = Vector(rowDelta * factor, colDelta * factor)
     operator fun List<String>.get(point: Point) = getOrNull(point.row)?.getOrNull(point.col)
     operator fun Point.plus(vector: Vector) = Point(row + vector.rowDelta, col + vector.colDelta)
@@ -12,7 +13,7 @@ fun main() {
                     return false
                 }
             }
-            return true;
+            return true
         }
 
         val range = -1..1
@@ -28,14 +29,26 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val neighbors = setOf('M', 'S')
+        val topLeft = Vector(-1, -1)
+        val topRight = Vector(-1, 1)
+        val bottomLeft = Vector(1, -1)
+        val bottomRight = Vector(1, 1)
+        return input.indices.sumOf { row ->
+            input[row].indices.count { col ->
+                val middle = Point(row, col)
+                input[middle] == 'A'
+                        && setOf(input[middle + topLeft], input[middle + bottomRight]) == neighbors
+                        && setOf(input[middle + topRight], input[middle + bottomLeft]) == neighbors
+            }
+        }
     }
 
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 18)
-//    check(part2(testInput) == 4)
+    check(part2(testInput) == 9)
 
     val input = readInput("Day04")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
