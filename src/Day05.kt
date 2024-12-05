a18cd54eee10b955427741ff27eb6cc37c7038ea
Marcel Joss <marceljoss@hotmail.ch>
2024-12-05 16:53:56 +0100

    Day 5 part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -8,32 +8,52 @@ fun main() {
         line.split(',').map { it.toInt() }
     }
 
+    fun List<Int>.isCorrectlyOrdered(rules: Map<Int, List<Int>>) = allIndexed { index, page ->
+        val otherPages = rules[page] ?: emptyList()
+        otherPages.all {
+            val otherIndex = lastIndexOf(it)
+            otherIndex == -1 || otherIndex > index
+        }
+    }
+
     fun part1(input: List<String>): Int {
         val (rulesInput, updatesInput) = input.split { it.isBlank() }
         val rules = readRules(rulesInput)
         val updates = readUpdates(updatesInput)
-        return updates.filter { update ->
-            update.allIndexed { index, page ->
-                val otherPages = rules[page] ?: emptyList()
-                otherPages.all {
-                    val otherIndex = update.lastIndexOf(it)
-                    otherIndex == -1 || otherIndex > index
-                }
-            }
-        }.sumOf {
+        return updates.filter { it.isCorrectlyOrdered(rules) }.sumOf {
             it[it.size / 2]
         }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val (rulesInput, updatesInput) = input.split { it.isBlank() }
+        val rules = readRules(rulesInput)
+        val updates = readUpdates(updatesInput)
+        return updates.filter {
+            !it.isCorrectlyOrdered(rules)
+        }.map { update ->
+            val newUpdate = update.toMutableList()
+            update.forEach { page ->
+                val otherPages = rules[page] ?: emptyList()
+                val currentIndex = newUpdate.indexOf(page)
+                otherPages.asSequence().map { newUpdate.indexOf(it) }.filter { it != -1 }.minOrNull()?.let {
+                    if (it < currentIndex) {
+                        newUpdate.removeAt(currentIndex)
+                        newUpdate.add(it, page)
+                    }
+                }
+            }
+            newUpdate
+        }.sumOf {
+            it[it.size / 2]
+        }
     }
 
     val testInput = readInput("Day05_test")
     check(part1(testInput) == 143)
-//    check(part2(testInput) == 9)
+    check(part2(testInput) == 123)
 
     val input = readInput("Day05")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
