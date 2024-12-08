dec107ffc2207966c255bce19198fffa5feb5f62
Marcel Joss <marceljoss@hotmail.ch>
2024-12-08 10:52:35 +0100

    Day 8 part 2

--- src/Day08.kt
+++ src/Day08.kt
@@ -29,13 +29,35 @@ fun main() {
         }.toSet().size
     }
 
-//    fun part2(input: List<String>): Int = 0
+    fun Point.next(rowDelta: Int, colDelta: Int) = Point(row + rowDelta, col + colDelta)
+
+    fun part2(input: List<String>): Int {
+        val rowRange = input.indices
+        val colRange = input.first().indices
+        val antennas = readAntennas(input)
+        val positionsByFrequency = antennas.groupBy({ it.frequency }, { it.position })
+        return positionsByFrequency.values.flatMap { positions ->
+            positions.flatMap { start ->
+                positions.asSequence().filter { it != start }.flatMap {
+                    val rowDelta = it.row - start.row
+                    val colDelta = it.col - start.col
+                    sequence {
+                        var current = it
+                        while (current.row in rowRange && current.col in colRange) {
+                            yield(current)
+                            current = current.next(rowDelta, colDelta)
+                        }
+                    }
+                }
+            }
+        }.toSet().size
+    }
 
     val testInput = readInput("Day08_test")
     check(part1(testInput) == 14)
-//    check(part2(testInput) == 11387L)
+    check(part2(testInput) == 34)
 
     val input = readInput("Day08")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
