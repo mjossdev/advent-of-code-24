0b2438bc42ebac568ef867cff3653211c6b0935e
Marcel Joss <marceljoss@hotmail.ch>
2024-12-22 13:49:48 +0100

    Day22 part 2

--- src/Day22.kt
+++ src/Day22.kt
@@ -2,29 +2,48 @@ fun main() {
     fun Long.prune() = this % 16777216
     fun Long.mix(transform: (Long) -> Long) = transform(this) xor this
 
-    fun Long.evolve(steps: Int): Long {
-        var secret = this
-        repeat(steps) {
+    fun Long.evolve(): Sequence<Long> = sequence {
+        var secret = this@evolve
+        while (true) {
+            yield(secret)
             secret = secret.mix { it shl 6 }.prune()
                 .mix { it shr 5 }.prune()
                 .mix { it shl 11 }.prune()
         }
-        return secret
     }
 
+    fun Long.toPrice(): Int = toString().last().digitToInt()
+
     fun part1(input: List<String>): Long = input.sumOf {
-        it.toLong().evolve(2000)
+        it.toLong().evolve().drop(2000).first()
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val priceMaps = input.map { initialSecret ->
+            buildMap {
+                initialSecret.toLong().evolve()
+                    .take(2001)
+                    .map { it.toPrice() }
+                    .zipWithNext()
+                    .windowed(4)
+                    .forEach {
+                        val changes = it.map { (prev, next) -> next - prev }
+                        if (changes !in this) {
+                            this[changes] = it.last().second
+                        }
+                    }
+            }
+        }
+        val allSequences = priceMaps.flatMap { it.keys }.toSet()
+        return allSequences.maxOf {sequence ->
+            priceMaps.sumOf { it[sequence] ?: 0 }
+        }
     }
 
-    val testInput = readInput("Day22_test")
-    check(part1(testInput) == 37327623L)
-//    check(part2(testInput) == 16)
+    check(part1(readInput("Day22_test1")) == 37327623L)
+    check(part2(readInput("Day22_test2")) == 23)
 
     val input = readInput("Day22")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
