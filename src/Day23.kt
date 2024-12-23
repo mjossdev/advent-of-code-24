4ec6bc3b03bfb37c2b06ef0a4d6497a5ad387b7d
Marcel Joss <marceljoss@hotmail.ch>
2024-12-23 10:55:04 +0100

    Day23 part 2

--- src/Day23.kt
+++ src/Day23.kt
@@ -27,15 +27,41 @@ fun main() {
         return triples.size
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): String {
+        val networkMap = input.toNetworkMap()
+        val bigOnesFirst = networkMap.toList().sortedBy { it.second.size }
+
+        val cache = mutableMapOf<Pair<Set<String>, Set<String>>, Set<String>>()
+        fun findBiggestLan(current: Set<String>, candidates: Set<String>): Set<String> =
+            cache.getOrPut(current to candidates) {
+                if (candidates.isEmpty()) {
+                    current
+                } else {
+                    candidates.asSequence()
+                        .filter { candidate -> current.all { candidate in networkMap.getValue(it) } }
+                        .map { findBiggestLan(current + it, candidates - it) }
+                        .maxByOrNull { it.size } ?: current
+                }
+            }
+
+        var biggestLan = emptySet<String>()
+        for ((first, others) in bigOnesFirst) {
+            if (others.size + 1 < biggestLan.size) {
+                break
+            }
+            val lan = findBiggestLan(setOf(first), others)
+            if (lan.size > biggestLan.size) {
+                biggestLan = lan
+            }
+        }
+        return biggestLan.sorted().joinToString(",")
     }
 
     val testInput = readInput("Day23_test")
     check(part1(testInput) == 7)
-//    check(part2(testInput) == 16L)
+    check(part2(testInput) == "co,de,ka,ta")
 
     val input = readInput("Day23")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
